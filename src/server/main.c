eb117dab36b9672f1a934b8b4006a2a82621e76a
ChillerDragon <chillerdragon@gmail.com>
2025-07-07 09:30:19 +0200

    random attempt in making the server chill and use a fixed tick rate

--- src/server/main.c
+++ src/server/main.c
@@ -1,7 +1,4 @@
-#include <errno.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -9,9 +6,13 @@
 #include <shared/logger.h>
 #include <shared/system.h>
 
+#include <server/system.h>
+
 #include "server_state.h"
 
 #include "server.h"
+
+#define SERVER_TICK_RATE 2000000
 
 int main() {
 	log_info("server", "starting ...");
@@ -19,7 +20,21 @@ int main() {
 	ServerState server;
 	server_init(&server);
 
+	uint64_t last_tick = time_get();
+
 	while(1) {
-		server_tick(&server);
+		const uint64_t now = time_get();
+		int ticks_behind = -1;
+		while((now - last_tick) >= SERVER_TICK_RATE) {
+			log_info("server", "tick %ld", last_tick);
+			server_tick(&server);
+			last_tick += SERVER_TICK_RATE;
+			ticks_behind++;
+		}
+		if(ticks_behind > 0) {
+			log_error("server", "%d ticks behind", ticks_behind);
+		}
+
+		// TODO: move networking read out of the fixed tick rate?
 	}
 }
