88b40e60ab32a0394d138de50e970669b5832e72
ChillerDragon <chillerdragon@gmail.com>
2025-08-04 14:35:48 +0200

    show player ids and pos

--- src/client/character.c
+++ src/client/character.c
@@ -1,6 +1,7 @@
 #include <SDL3/SDL_stdinc.h>
 
 #include <game/character.h>
+#include <stdio.h>
 
 #include "character.h"
 
@@ -14,4 +15,9 @@ void character_draw(Character *character, Camera *camera, SDL_Renderer *renderer
 	camera_frect_world_to_screen(camera, &r);
 	SDL_SetRenderDrawColor(renderer, 0, 128, 0, SDL_ALPHA_OPAQUE);
 	SDL_RenderFillRect(renderer, &r);
+
+	char buf[512];
+	snprintf(buf, sizeof(buf), "cid=%d x=%d y=%d", character->client_id, character->pos.x, character->pos.y);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderDebugText(renderer, r.x, r.y - 10, buf);
 }
